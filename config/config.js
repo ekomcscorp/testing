fc89a0a6eab47c6aeb98e38073640053616cd615
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2026-03-13 11:26:37 +0700

    ubah port ke 3306

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '12345678',
-    database: process.env.DB_NAME || 'kaiadmin2',
-    host: process.env.DB_HOST || '127.0.0.1', //185.185.185.185
-    port: Number(process.env.DB_PORT) || 3307, // ✅ PENTING!
-    dialect: 'mysql'
-  },
+  // development: {
+  //   username: process.env.DB_USERNAME || 'root',
+  //   password: process.env.DB_PASSWORD || '12345678',
+  //   database: process.env.DB_NAME || 'kaiadmin2',
+  //   host: process.env.DB_HOST || '127.0.0.1', //185.185.185.185
+  //   port: Number(process.env.DB_PORT) || 3307, // ✅ PENTING!
+  //   dialect: 'mysql'
+  // },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306, // ✅ PENTING!
+    port: Number(process.env.DB_PORT), // ✅ PENTING!
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
