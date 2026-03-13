b7a8aa57e4e60263d1117cf69ad24259b4a25d12
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2026-03-13 11:33:25 +0700

    config

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '12345678',
-    database: process.env.DB_NAME || 'kaiadmin2',
-    host: process.env.DB_HOST || '127.0.0.1', //185.185.185.185
-    port: Number(process.env.DB_PORT) || 3307, // ✅ PENTING!
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST , //185.185.185.185
+    port: Number(process.env.DB_PORT), // ✅ PENTING!
     dialect: 'mysql'
   },
   production: {
